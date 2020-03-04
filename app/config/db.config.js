a31939b8844ac2e1d186021b78d3efc487dab813
Facundo <fsavat@folderit.net>
2020-03-04 04:01:04 -0300

    Se configuró la conexión a la Db en Heroku

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "detectormutante",
-    PASSWORD: "detectormutante",
-    DB: "detectormutante",
+    HOST: "us-cdbr-iron-east-04.cleardb.net",
+    USER: "b1d5091901c725",
+    PASSWORD: "70064d4a",
+    DB: "heroku_b248512a8c240a2",
     dialect: "mysql",
     pool: {
       max: 1000,
